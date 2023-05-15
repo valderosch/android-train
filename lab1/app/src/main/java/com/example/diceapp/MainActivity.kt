5c3f3478bf65d90d34a38e15f1480c2f7aa4ebae
valderosch <phonelix96@gmail.com>
2023-05-16 00:05:38 +0300

    visualised
    Added visualisation for app

--- lab1/app/src/main/java/com/example/diceapp/MainActivity.kt
+++ lab1/app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.diceapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * This activity allows the user to roll a dice and view the result
@@ -15,25 +15,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Starting Dice Roll
+        rollDice()
+
         //RollButton assign
         val rollButton: Button = findViewById(R.id.button2)
         rollButton.setOnClickListener {
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            //Calling dice roll logic function
             rollDice()
         }
     }
 
     //Roll the dice and update the screen with the result.
     private fun rollDice() {
-        val dice = Dice(10)
+        val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+        //Assign image and result
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_1
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
 
+/**
+ * This activity returns random result in current range ( 1 - (end value))
+ */
 class Dice(private val numSides: Int) {
-
+    // Roll method that returns random value
     fun roll(): Int {
         return (1..numSides).random()
     }

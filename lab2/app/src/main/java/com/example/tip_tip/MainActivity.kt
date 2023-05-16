0fd32af1f16e71e7d63f257ba6dcdf2be4880945
valderosch <phonelix96@gmail.com>
2023-05-16 10:20:26 +0300

    debugging

--- lab2/app/src/main/java/com/example/tip_tip/MainActivity.kt
+++ lab2/app/src/main/java/com/example/tip_tip/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     //Calculate tips
     fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull()
 
         //RadioButton binding and getting selectd value
         val selectedId = binding.tipOptions.checkedRadioButtonId
@@ -29,6 +29,10 @@ class MainActivity : AppCompatActivity() {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
+        }
+
+        if (cost == null) {
+            return
         }
 
         var tip = tipPercentage * cost
